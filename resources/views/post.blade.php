3c01b397e995caf8b37aa3f2e4b8b9eb7fa4a603
Bernardo <b.hulsman@tripix.nl>
2019-01-23 12:17:44 +0100

    Fixed title line height.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container mx-auto">
-        <h1 class="mb-2 font-semibold text-5xl">{{ $post->title }}</h1>
+        <h1 class="mb-2 font-semibold text-5xl leading-none">{{ $post->title }}</h1>
         <div class="border-b-2 border-grey-darker mb-4 pb-4 text-xs text-grey uppercase">{{ $post->date->format('j F Y') }}</div>
         <div class="content">
             {!! $post->contents !!}
